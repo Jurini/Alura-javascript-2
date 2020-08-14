103d9f11d64341bad815a1544a4251be9475bcae
Jurini <curvello_@hotmail.com>
2020-08-14 15:24:41 -0300

    Alterações relacionadas a View

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,6 +7,9 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._negociacoesView.update(this._listaNegociacoes);
 
     }
 
@@ -15,9 +18,9 @@ class NegociacaoController {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
-
-        console.log(this._listaNegociacoes.negociacoes);
+        
     }
 
     _criaNegociacao() {

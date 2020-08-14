a03774bc03410b1ca54793d351becb86142de5c8
Jurini <curvello_@hotmail.com>
2020-08-13 22:20:52 -0300

    Regex para validação do formato da data

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,18 +1,25 @@
 class DateHelper {
 
-    dataParaTexto(data) {
+    constructor() {
 
-        return data.getDate()
-            + '/' + (data.getMonth() + 1)
-            + '/' + data.getFullYear();
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static dataParaTexto(data) {
+
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
 
     }
 
-    textoParaData(texto) {
+    static textoParaData(texto) {
 
         // ... dismenbrando o array , Split >> transforma a String em array
         // => flecha , forma menos verbosa de declarar funções ES2015
-        // arrow function já faz o return da operação   
+        // arrow function já faz o return da operação 
+
+        //Regex()
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto))
+            throw new Error('Deve estar no formato aaaa-mm-dd');
 
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
 

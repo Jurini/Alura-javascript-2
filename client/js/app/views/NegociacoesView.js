c3a456078e312ce5f226fff92676d5c830c1a747
Jurini <curvello_@hotmail.com>
2020-08-14 15:26:54 -0300

    Renderizando a tabela e Calculando volume total

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,12 +1,12 @@
-class NegociacoesViews {
+class NegociacoesView {
 
-    constructor(elemento){
+    constructor(elemento) {
 
         this._elemento = elemento;
 
     }
 
-    template() {
+    _template(model) {
 
         return `
         <table class="table table-hover table-bordered">
@@ -20,11 +20,29 @@ class NegociacoesViews {
             </thead>
 
             <tbody>
+            ${model.negociacoes.map((n) => `
+
+                 <tr>
+                     <td>${DateHelper.dataParaTexto(n.data)}</td>
+                     <td>${n.quantidade}</td>
+                     <td>${n.valor}</td>
+                     <td>${n.volume}</td>
+                 </tr>
+                
+            `).join('')}
             </tbody>
 
             <tfoot>
+                <td colspan="3"></td>
+                <td>
+                    ${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
+                </td>
             </tfoot>
         </table>
                 `;
+    }
+
+    update(model) {
+        this._elemento.innerHTML = this._template(model);
     }
 }
